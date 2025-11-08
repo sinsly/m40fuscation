f58e8691f74bf524ab39e86e41c2116328767975
sins <104468192+sinsly@users.noreply.github.com>
2025-11-07 22:22:30 -0500

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,2 @@
+local src = loadstring(game:HttpGet("https://raw.githubusercontent.com/sinsly/m40fuscation/main/test.lua"))()
+local dcm = loadstring(game:HttpGet("https://raw.githubusercontent.com/sinsly/m40fuscation/main/connect.lua"))()
